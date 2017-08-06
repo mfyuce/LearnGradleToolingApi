e2510ff28d5521ef4ec29ed7923ae1aa8818eb8e
mehmet <mehmet.yuce@bitekanalytics.com>
2017-08-06 04:35:07 +0300

    -working with the latest version of gradle -no need for libs

--- src/test/java/com/study/gradle/tooling/api/GradleConnectorTest.java
+++ src/test/java/com/study/gradle/tooling/api/GradleConnectorTest.java
@@ -11,14 +11,14 @@ import static junit.framework.TestCase.assertEquals;
 
 public class GradleConnectorTest {
     public static final String CURRENT_PROJECT_DIR = ".";
-    private String projectDir = "./src/main/resources/gradleModule";
+    private String projectDir = "./gradleModule";
     private GradleConnector connector = new GradleConnector( projectDir);
 
     @Test
     public void getGradleVersion() throws IOException, InterruptedException {
         String version = connector.getGradleVersion();
 
-        assertEquals("2.1",version);
+        assertEquals("4.0.2",version);
     }
 
 
@@ -39,7 +39,7 @@ public class GradleConnectorTest {
     @Test
     public void getGradleTasksForProject(){
         List<String> tasks = connector.getGradleTaskNames();
-        List<String> expected = Arrays.asList("assemble", "build", "buildDependents", "buildNeeded", "check", "classes", "clean", "cleanIdea", "cleanIdeaModule", "cleanIdeaProject", "cleanIdeaWorkspace", "compileJava", "compileTestJava", "copyDepJars", "idea", "ideaModule", "ideaProject", "ideaWorkspace", "jar", "javadoc", "processResources", "processTestResources", "test", "testClasses", "war");
+        List<String> expected = Arrays.asList("assemble", "build", "buildDependents", "buildEnvironment", "buildNeeded", "check", "classes", "clean", "cleanIdea", "cleanIdeaModule", "cleanIdeaProject", "cleanIdeaWorkspace", "compileJava", "compileTestJava", "components", "copyDepJars", "dependencies", "dependencyInsight", "dependentComponents", "help", "idea", "ideaModule", "ideaProject", "ideaWorkspace", "init", "jar", "javadoc", "model", "processResources", "processTestResources", "projects", "properties", "tasks", "test", "testClasses", "war", "wrapper");
 
          assertEquals(expected, tasks);
     }
@@ -48,17 +48,7 @@ public class GradleConnectorTest {
     @Test
     public void getDependenciesForProject(){
         List<String> dependencies = connector.getProjectDependencyNames();
-        List<String> expected = Arrays.asList("spring-core-4.2.3.RELEASE.jar",
-                "spring-rabbit-1.5.2.RELEASE.jar","commons-logging-1.2.jar",
-                "spring-messaging-4.2.2.RELEASE.jar","spring-retry-1.1.2.RELEASE.jar",
-                "spring-amqp-1.5.2.RELEASE.jar","spring-tx-4.2.2.RELEASE.jar",
-                "http-client-1.0.0.RELEASE.jar","spring-web-4.2.2.RELEASE.jar",
-                "amqp-client-3.5.6.jar","spring-context-4.2.2.RELEASE.jar",
-                "spring-beans-4.2.2.RELEASE.jar","httpclient-4.3.6.jar",
-                "jackson-databind-2.5.1.jar","spring-aop-4.2.2.RELEASE.jar",
-                "spring-expression-4.2.2.RELEASE.jar","httpcore-4.3.3.jar",
-                "commons-codec-1.6.jar","jackson-annotations-2.5.0.jar",
-                "jackson-core-2.5.1.jar","aopalliance-1.0.jar");
+        List<String> expected = Arrays.asList("spring-rabbit-1.5.2.RELEASE.jar", "spring-messaging-4.2.2.RELEASE.jar", "spring-retry-1.1.2.RELEASE.jar", "spring-amqp-1.5.2.RELEASE.jar", "spring-tx-4.2.2.RELEASE.jar", "spring-web-4.2.2.RELEASE.jar", "spring-context-4.2.2.RELEASE.jar", "spring-aop-4.2.2.RELEASE.jar", "spring-beans-4.2.2.RELEASE.jar", "spring-expression-4.2.2.RELEASE.jar", "spring-core-4.2.3.RELEASE.jar", "http-client-1.0.0.RELEASE.jar", "httpclient-4.3.6.jar", "commons-logging-1.2.jar", "amqp-client-3.5.6.jar", "jackson-databind-2.5.1.jar", "httpcore-4.3.3.jar", "commons-codec-1.6.jar", "jackson-annotations-2.5.0.jar", "jackson-core-2.5.1.jar", "aopalliance-1.0.jar", "spring-rabbit-1.5.2.RELEASE.jar", "spring-messaging-4.2.2.RELEASE.jar", "spring-retry-1.1.2.RELEASE.jar", "spring-amqp-1.5.2.RELEASE.jar", "spring-tx-4.2.2.RELEASE.jar", "spring-web-4.2.2.RELEASE.jar", "spring-context-4.2.2.RELEASE.jar", "spring-aop-4.2.2.RELEASE.jar", "spring-beans-4.2.2.RELEASE.jar", "spring-expression-4.2.2.RELEASE.jar", "spring-core-4.2.3.RELEASE.jar", "http-client-1.0.0.RELEASE.jar", "httpclient-4.3.6.jar", "commons-logging-1.2.jar", "amqp-client-3.5.6.jar", "jackson-databind-2.5.1.jar", "httpcore-4.3.3.jar", "commons-codec-1.6.jar", "jackson-annotations-2.5.0.jar", "jackson-core-2.5.1.jar", "aopalliance-1.0.jar", "spring-rabbit-1.5.2.RELEASE.jar", "spring-messaging-4.2.2.RELEASE.jar", "spring-retry-1.1.2.RELEASE.jar", "spring-amqp-1.5.2.RELEASE.jar", "spring-tx-4.2.2.RELEASE.jar", "spring-web-4.2.2.RELEASE.jar", "spring-context-4.2.2.RELEASE.jar", "spring-aop-4.2.2.RELEASE.jar", "spring-beans-4.2.2.RELEASE.jar", "spring-expression-4.2.2.RELEASE.jar", "spring-core-4.2.3.RELEASE.jar", "http-client-1.0.0.RELEASE.jar", "httpclient-4.3.6.jar", "commons-logging-1.2.jar", "amqp-client-3.5.6.jar", "jackson-databind-2.5.1.jar", "httpcore-4.3.3.jar", "commons-codec-1.6.jar", "jackson-annotations-2.5.0.jar", "jackson-core-2.5.1.jar", "aopalliance-1.0.jar");
 
         assertEquals(expected, dependencies);
     }
@@ -69,7 +59,7 @@ public class GradleConnectorTest {
 
 //        assertTrue(connector.buildProject());
         assertTrue(connector.buildProject("compileJava"));
-        assertEquals(Arrays.asList("junit-4.4.jar", "gradle-core-2.1.jar", "gradle-tooling-api-2.1.jar", "gradle-resources-2.1.jar", "gradle-messaging-2.1.jar", "gradle-base-services-2.1.jar", "gradle-wrapper-2.1.jar", "guava-jdk5-17.0.jar", "slf4j-api-1.7.5.jar", "junit-4.4.jar", "gradle-core-2.1.jar", "gradle-tooling-api-2.1.jar", "gradle-resources-2.1.jar", "gradle-messaging-2.1.jar", "gradle-base-services-2.1.jar", "gradle-wrapper-2.1.jar", "guava-jdk5-17.0.jar", "slf4j-api-1.7.5.jar", "junit-4.4.jar", "gradle-core-2.1.jar", "gradle-tooling-api-2.1.jar", "gradle-resources-2.1.jar", "gradle-messaging-2.1.jar", "gradle-base-services-2.1.jar", "gradle-wrapper-2.1.jar", "guava-jdk5-17.0.jar", "slf4j-api-1.7.5.jar"), connector.getProjectDependencyNames());
+        assertEquals(Arrays.asList("junit-4.4.jar", "gradle-tooling-api-4.0.2.jar", "slf4j-api-1.7.10.jar", "junit-4.4.jar", "gradle-tooling-api-4.0.2.jar", "slf4j-api-1.7.10.jar", "junit-4.4.jar", "gradle-tooling-api-4.0.2.jar", "slf4j-api-1.7.10.jar"), connector.getProjectDependencyNames());
 
         List<String> expected = Arrays.asList("assemble", "build", "buildDependents", "buildEnvironment", "buildNeeded", "check", "classes", "clean", "cleanIdea", "cleanIdeaModule", "cleanIdeaProject", "cleanIdeaWorkspace", "compileJava", "compileTestJava", "components", "dependencies", "dependencyInsight", "dependentComponents", "help", "idea", "ideaModule", "ideaProject", "ideaWorkspace", "init", "jar", "javadoc", "model", "processResources", "processTestResources", "projects", "properties", "tasks", "test", "testClasses", "wrapper");
 
